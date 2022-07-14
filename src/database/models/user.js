a0b032813129150fc396280d8c042c8bf4760267
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-07-14 11:19:36 -0300

    fix: [req 02]

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -8,7 +8,8 @@ const createUser = (sequelize, DataTypes) => {
     password: DataTypes.STRING,
     image: DataTypes.STRING,
   }, {
-    tableName: 'Users'
+    tableName: 'Users',
+    timestamps: false
   });
 
   return User;
