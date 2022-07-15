a5aaab13abaa707f9ba9d77d4a71b6b6a2461477
Thais Kotovicz <thais_kotovicz@hotmail.com>
2022-07-15 17:54:23 -0300

    feat: [req 07] Cria model Category

---
+++ src/database/models/category.js
@@ -0,0 +1,18 @@
+const sequelize = require('sequelize');
+
+const createCategory = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+    name: DataTypes.STRING,
+  }, {
+      tableName: 'Categories',
+      timestamps: false
+  });
+
+  return Category;
+}
+
+module.exports = createCategory;
